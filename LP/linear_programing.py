2d16f72a5d94bfda3f246458ac16167c2117a865
shiranAziz <shiran.aziz@gmail.com>
2020-03-12 18:13:28 +0200

    change

--- LP/linear_programing.py
+++ LP/linear_programing.py
@@ -1,207 +1,42 @@
+from LP.linear_programing_tools import linearPrograming
 import numpy as np
 
 
-class linearPrograming:
-
-    def __init__(self, matrix_A, vector_b, vector_c):
-        self.epsilon = 0.01
-        self.solution = {-2: 'unbounded solution',
-                    -3: 'There is no solution', -1: 'optimal solution'}
-        self.A = matrix_A
-        self.b = vector_b
-        self.c = vector_c
-
-        shape = self.A.shape
-        #also num of slack vars
-        self.num_rows = shape[0]
-        self.num_cols = shape[1]
-
-        self.result = np.zeros(self.c.shape)
-
-        self.Base = np.identity(self.num_rows)
-        self.non_bases_vars = np.arange(start=1, stop=self.num_cols+1)
-        self.bases_vars = np.arange(start=self.num_cols+1,
-                                        stop=self.num_cols+1+self.num_rows)
-        self.AN = self.A
-        self.cN = np.copy(vector_c)
-        self.cB = np.zeros(self.num_rows)
-        self.xB = self.b
-
-        self.eta_matrix = []
-        self.leaving_vars = []
-
-    def swap_entering_leaving(self, entering_var, leaving_var):
-        #update base and AN
-        temp_B = np.copy(self.Base[:,leaving_var])
-        self.Base[:,leaving_var] = self.AN[:,entering_var]
-        self.AN[:,entering_var] = temp_B
-
-        #update cB and cN
-        temp_c = np.copy(self.cB[leaving_var])
-        self.cB[leaving_var] = self.cN[entering_var]
-        self.cN[entering_var] = temp_c
-
-        #update bases vars and non_bases_vars
-        entering_var_index = self.non_bases_vars[entering_var]
-        leaving_var_index = self.bases_vars[leaving_var]
-        self.bases_vars[leaving_var] = entering_var_index
-        self.non_bases_vars[entering_var] = leaving_var_index
-
-    def is_optimal_solution(self, coefficient):
-        for i in coefficient:
-            if i > 0:
-                return False
-        return True
-
-    def is_unbounded_solution(self, t_vector):
-        return not np.any(t_vector >= 0)
-
-    def picking_entering_var_dantzig(self, y):
-        coefficient = self.cN - np.dot(y,self.AN)
-        for i in range(len(coefficient)):
-            if coefficient[i]< self.epsilon:
-                coefficient[i] = 0
-        if self.is_optimal_solution(coefficient):
-            return -1
-        return np.argmax(coefficient)
-
-    def picking_entering_var_bland(self,y):
-        coefficient = self.cN - np.dot(y, self.AN)
-        for i in range(len(coefficient)):
-            if coefficient[i] < self.epsilon:
-                coefficient[i] = 0
-        if self.is_optimal_solution(coefficient):
-            return -1
-        for i in range(len(coefficient)):
-            if coefficient[i] > 0:
-                return i
-
-    def picking_leaving_var(self, d):
-        t_vector = np.zeros(d.shape)
-        for i in range(len(d)):
-            if d[i] != 0:
-                t_vector[i] = self.xB[i] / d[i]
-        if self.is_unbounded_solution(t_vector):
-            return -2
-        min = t_vector[0]
-        min_index = 0
-        for i in range(len(t_vector)):
-            if t_vector[i] > 0 and min > t_vector[i]:
-                min = t_vector[i]
-                min_index = i
-        return min_index
+def is_b_negative(b):
+    for i in b:
+        if i < 0:
+            return True
+    return False
 
 
-    def update_result(self, leaving_var, d):
-        t_vector = np.zeros(d.shape)
-        for i in range(len(d)):
-            if d[i] != 0:
-                t_vector[i] = self.xB[i] / d[i]
-        t = t_vector[leaving_var]
-        d_with_t = (np.ones(t_vector.shape[0])*t)*d
-        self.xB = self.xB - d_with_t
-        self.xB[leaving_var] = t
+def simplex_result(matrix_A, vector_b, vector_c):
+    # if is_b_negative(vector_b):
+    #     min_b = vector_b[0]
+    #     for i in vector_b:
+    #         if min_b > i:
+    #             min_b = i
+    #
+    #     A = np.zeros((matrix_A.shape[0], matrix_A.shape[1]+1))
+    #     x = -1 * np.ones(matrix_A.shape[0])
+    #     A = np.hstack((x[:,np.newaxis], matrix_A))
+    #     C = np.zeros(len(vector_c))
+    #     C[0] = -1
+    #     preprocess = linearPrograming(A, vector_b, C)
+    #     d = np.copy(matrix_A[:, matrix_A.shape[1]+1]) # TODO: bug here
+    #     preprocess.swap_entering_leaving(i, 0)
+    #     preprocess.update_result(d, 0)
+    #     res = preprocess.run_simplex()
+    #     if res != 0:
+    #         return "no solution"
 
-    def BTRAN(self, d, leaving_var):
-        self.leaving_vars.append(leaving_var)
-        eta = np.identity(self.num_rows)
-        eta[:,leaving_var] = np.copy(d)
-        self.eta_matrix.append(np.copy(eta))
-        known_result = self.cB
-        for i in range(len(self.leaving_vars)-1, -1 , -1):
-            known_result = self.single_BTRAN(known_result,i)
-        return known_result
-
-    def single_BTRAN(self,known_result, i ):
-        y = np.zeros(self.num_rows)
-        eta_vector_index = self.leaving_vars[i]
-        eta_vector = self.eta_matrix[i][:,eta_vector_index]
-        factor = 1 / eta_vector[eta_vector_index]
-        for j in range(self.num_rows):
-            if j != eta_vector_index:
-                y[j] = known_result[j]
-            else:
-                y[eta_vector_index] = factor * known_result[eta_vector_index]
-                for k in range(self.num_rows):
-                    if k != eta_vector_index:
-                        y[eta_vector_index] -= factor*eta_vector[k]*known_result[k]
-        return y
-
-    def FTRAN(self, known_result):
-        for i in range(0, len(self.leaving_vars)):
-            known_result = self.single_FTRAN(known_result, i)
-        return known_result
-
-    def single_FTRAN(self, known_result, i):
-        d = np.zeros(self.num_rows)
-        eta_vector_index = self.leaving_vars[i]
-        eta_vector = self.eta_matrix[i][:,eta_vector_index]
-        factor = (1 / eta_vector[eta_vector_index])
-        d[eta_vector_index] = factor * known_result[eta_vector_index]
-        for j in range(self.num_rows):
-            if j != eta_vector_index:
-                d[j] = known_result[j]-eta_vector[j]*d[eta_vector_index]
-        return d
-
-    def run_simplex(self):
-        #first iteration is different because B=I and cB = 0
-        # we don't need FTRAN and BTRAN then
-        # y*Base = cB -> y = 0
-        y = np.zeros(self.num_rows)
-        entering_var = self.picking_entering_var_bland(y)
-        # Base*d = a = self.AN[:entering_var] - > d = a
-        d = np.copy(self.AN[:,entering_var])
-        leaving_var = self.picking_leaving_var(d)
-        self.swap_entering_leaving(entering_var, leaving_var)
-        self.update_result(leaving_var, d)
-        while True:
-            y = self.BTRAN(d,leaving_var)
-            entering_var = self.picking_entering_var_bland(y)
-            if entering_var < 0:
-                return entering_var
-            a = np.copy(self.AN[:,entering_var])
-            d = self.FTRAN(a)
-            leaving_var = self.picking_leaving_var(d)
-            if leaving_var < 0:
-                return leaving_var
-            self.swap_entering_leaving(entering_var, leaving_var)
-            self.update_result(leaving_var,d)
-
-    def simplex_result(self):
-        result = self.run_simplex()
-        if result != -1:
-            return self.solution[result]
-        elif result == -1:
-            x_max = np.zeros(self.num_rows + self.num_cols)
-            for i in range(self.num_rows):
-                x_max[self.bases_vars[i]-1] = self.xB[i]
-            pad = np.zeros(self.num_rows)
-            c_new = np.hstack((self.c, pad))
-            return np.dot(c_new, x_max)
-
-
-
-
-
-
-
-
-    # def negative_b(self):
-
-# def find_basic_vars(self):
-#     num_of_one = 0
-#     flag = 0
-#     is_basic = np.zeros(self.num_cols)
-#     for j in range(self.num_cols):
-#         for i in range(self.num_rows):
-#             if self.A[i][j] == 1:
-#                 num_of_one += 1
-#             elif self.A[i][j] != 0:
-#                 flag = 1
-#                 break
-#         if num_of_one == 1 and flag == 0:
-#             is_basic[j] = 1
-#         flag = 0
-#     return is_basic
-
-
+    s = linearPrograming(matrix_A, vector_b, vector_c)
+    result = s.run_simplex()
+    if result != -1:
+        return s.solution[result]
+    elif result == -1:
+        x_max = np.zeros(s.num_rows + s.num_cols)
+        for i in range(s.num_rows):
+            x_max[s.bases_vars[i] - 1] = s.xB[i]
+        pad = np.zeros(s.num_rows)
+        c_new = np.hstack((s.c, pad))
+        return np.dot(c_new, x_max)
---
+++ LP/linear_programing_tools.py
@@ -0,0 +1,221 @@
+import numpy as np
+from LP.LU_factorization import LU_factorization
+
+
+class linearPrograming:
+    TRESHOLD_ETA = 100
+
+
+    def __init__(self, matrix_A, vector_b, vector_c):
+        self.epsilon = 0.01
+        self.solution = {-2: 'unbounded solution',
+                    -3: 'There is no solution', -1: 'optimal solution'}
+        self.A = np.copy(matrix_A)
+        self.b = np.copy(vector_b)
+        self.c = np.copy(vector_c)
+
+        shape = self.A.shape
+        #also num of slack vars
+        self.num_rows = shape[0]
+        self.num_cols = shape[1]
+
+        self.result = np.zeros(self.c.shape)
+
+        self.Base = np.identity(self.num_rows)
+        self.non_bases_vars = np.arange(start=1, stop=self.num_cols+1)
+        self.bases_vars = np.arange(start=self.num_cols+1,
+                                        stop=self.num_cols+1+self.num_rows)
+        self.AN = self.A
+        self.cN = np.copy(vector_c)
+        self.cB = np.zeros(self.num_rows)
+        self.xB = self.b
+
+        self.eta_matrix = []
+        self.leaving_vars = []
+
+    def swap_entering_leaving(self, entering_var, leaving_var):
+        #update base and AN
+        temp_B = np.copy(self.Base[:,leaving_var])
+        self.Base[:,leaving_var] = self.AN[:,entering_var]
+        self.AN[:,entering_var] = temp_B
+
+        #update cB and cN
+        temp_c = np.copy(self.cB[leaving_var])
+        self.cB[leaving_var] = self.cN[entering_var]
+        self.cN[entering_var] = temp_c
+
+        #update bases vars and non_bases_vars
+        entering_var_index = self.non_bases_vars[entering_var]
+        leaving_var_index = self.bases_vars[leaving_var]
+        self.bases_vars[leaving_var] = entering_var_index
+        self.non_bases_vars[entering_var] = leaving_var_index
+
+    def is_optimal_solution(self, coefficient):
+        for i in coefficient:
+            if i > 0:
+                return False
+        return True
+
+    def is_unbounded_solution(self, t_vector):
+        return not np.any(t_vector >= 0)
+
+    def picking_entering_var_dantzig(self, y):
+        coefficient = self.cN - np.dot(y,self.AN)
+        for i in range(len(coefficient)):
+            if coefficient[i]< self.epsilon:
+                coefficient[i] = 0
+        if self.is_optimal_solution(coefficient):
+            return -1
+        return np.argmax(coefficient)
+
+    def picking_entering_var_bland(self,y):
+        coefficient = self.cN - np.dot(y, self.AN)
+        for i in range(len(coefficient)):
+            if coefficient[i] < self.epsilon:
+                coefficient[i] = 0
+        if self.is_optimal_solution(coefficient):
+            return -1
+        for i in range(len(coefficient)):
+            if coefficient[i] > 0:
+                return i
+
+    def picking_leaving_var(self, d):
+        t_vector = np.zeros(d.shape)
+        for i in range(len(d)):
+            if d[i] != 0:
+                t_vector[i] = self.xB[i] / d[i]
+        if self.is_unbounded_solution(t_vector):
+            return -2
+        min = t_vector[0]
+        min_index = 0
+        for i in range(len(t_vector)):
+            if t_vector[i] > 0 and min > t_vector[i]:
+                min = t_vector[i]
+                min_index = i
+        return min_index
+
+
+    def update_result(self, leaving_var, d):
+        t_vector = np.zeros(d.shape)
+        for i in range(len(d)):
+            if d[i] != 0:
+                t_vector[i] = self.xB[i] / d[i]
+        t = t_vector[leaving_var]
+        d_with_t = (np.ones(t_vector.shape[0])*t)*d
+        self.xB = self.xB - d_with_t
+        self.xB[leaving_var] = t
+
+    def BTRAN(self, d, leaving_var):
+        self.leaving_vars.append(leaving_var)
+        eta = np.identity(self.num_rows)
+        eta[:,leaving_var] = np.copy(d)
+        self.eta_matrix.append(np.copy(eta))
+        known_result = self.cB
+        for i in range(len(self.leaving_vars)-1, -1 , -1):
+            known_result = self.single_BTRAN(known_result,i)
+        return known_result
+
+    def single_BTRAN(self,known_result, i ):
+        y = np.zeros(self.num_rows)
+        eta_vector_index = self.leaving_vars[i]
+        eta_vector = self.eta_matrix[i][:,eta_vector_index]
+        factor = 0
+        if eta_vector[eta_vector_index] > self.epsilon:
+            factor = 1 / eta_vector[eta_vector_index]
+        for j in range(self.num_rows):
+            if j != eta_vector_index:
+                y[j] = known_result[j]
+            else:
+                y[eta_vector_index] = factor * known_result[eta_vector_index]
+                for k in range(self.num_rows):
+                    if k != eta_vector_index:
+                        y[eta_vector_index] -= factor*eta_vector[k]*known_result[k]
+        return y
+
+    def FTRAN(self, known_result):
+        for i in range(0, len(self.leaving_vars)):
+            known_result = self.single_FTRAN(known_result, i)
+        return known_result
+
+    def single_FTRAN(self, known_result, i):
+        d = np.zeros(self.num_rows)
+        eta_vector_index = self.leaving_vars[i]
+        eta_vector = self.eta_matrix[i][:,eta_vector_index]
+        factor = (1 / eta_vector[eta_vector_index])
+        d[eta_vector_index] = factor * known_result[eta_vector_index]
+        for j in range(self.num_rows):
+            if j != eta_vector_index:
+                d[j] = known_result[j]-eta_vector[j]*d[eta_vector_index]
+        return d
+
+    def run_simplex(self):
+        #first iteration is different because B=I and cB = 0
+        # we don't need FTRAN and BTRAN then
+        # y*Base = cB -> y = 0
+        y = np.zeros(self.num_rows)
+        entering_var = self.picking_entering_var_bland(y)
+        # Base*d = a = self.AN[:entering_var] - > d = a
+        d = np.copy(self.AN[:,entering_var])
+        leaving_var = self.picking_leaving_var(d)
+        self.swap_entering_leaving(entering_var, leaving_var)
+        self.update_result(leaving_var, d)
+        while True:
+            if len(self.eta_matrix) == self.TRESHOLD_ETA:
+                s = LU_factorization(self.Base)
+                self.Base, self.eta_matrix, self.leaving_vars = s.run_LU_factorization()
+            y = self.BTRAN(d,leaving_var)
+            entering_var = self.picking_entering_var_bland(y)
+            if entering_var < 0:
+                return entering_var
+            a = np.copy(self.AN[:,entering_var])
+            d = self.FTRAN(a)
+            leaving_var = self.picking_leaving_var(d)
+            if leaving_var < 0:
+                return leaving_var
+            self.swap_entering_leaving(entering_var, leaving_var)
+            self.update_result(leaving_var,d)
+
+    # def simplex_result(self):
+    #     if self.is_b_negative:
+    #         min_b = self.b[0]
+    #         for i in self.b:
+    #             if min_b > i:
+    #                 min_b = i
+    #
+    #     result = self.run_simplex()
+    #     if result != -1:
+    #         return self.solution[result]
+    #     elif result == -1:
+    #         x_max = np.zeros(self.num_rows + self.num_cols)
+    #         for i in range(self.num_rows):
+    #             x_max[self.bases_vars[i]-1] = self.xB[i]
+    #         pad = np.zeros(self.num_rows)
+    #         c_new = np.hstack((self.c, pad))
+    #         return np.dot(c_new, x_max)
+    #
+    #
+    #
+    #
+
+
+
+
+    # def negative_b(self):
+
+# def find_basic_vars(self):
+#     num_of_one = 0
+#     flag = 0
+#     is_basic = np.zeros(self.num_cols)
+#     for j in range(self.num_cols):
+#         for i in range(self.num_rows):
+#             if self.A[i][j] == 1:
+#                 num_of_one += 1
+#             elif self.A[i][j] != 0:
+#                 flag = 1
+#                 break
+#         if num_of_one == 1 and flag == 0:
+#             is_basic[j] = 1
+#         flag = 0
+#     return is_basic
+
+
