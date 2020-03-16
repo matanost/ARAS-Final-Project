0fe5c95f17e20e3e20adef46d4a8f81801d748e7
shiranAziz <shiran.aziz@gmail.com>
2020-03-16 20:10:49 +0200

    dsfsd

--- demo/smt_lp_demo.py
+++ demo/smt_lp_demo.py
@@ -2,42 +2,47 @@
 from SMT.SMTSolver import SMTSolver as SMT
 from SAT.CNF_formula import CNF_formula
 
-formulas = list()
-req_result = list()
-results = list()
-fails = set()
-smt = SMT()
-PASS = True
+def demo():
+    formulas = list()
+    req_result = list()
+    results = list()
+    fails = set()
+    smt = SMT()
+    PASS = True
 
-formulas.append("(a<=4)*(b<=3)")
-req_result.append(True)
-formulas.append("(-a>=-4)*(b<=3)")
-req_result.append(True)
-formulas.append("((3a1 + -a2 + 4a3 <=10)*(a1<=3))*(a2 <= 100)")
-req_result.append(True)
+    formulas.append("(a<=4)*(b<=3)")
+    req_result.append(True)
+    formulas.append("(-a>=-4)*(b<=3)")
+    req_result.append(True)
+    formulas.append("((3a1 + -a2 + 4a3 <=10)*(a1<=3))*(a2 <= 100)")
+    req_result.append(True)
 
-formulas.append("(a<=4)*(4<=a)")
-req_result.append(True)
+    formulas.append("(a<=4)*(4<=a)")
+    req_result.append(True)
 
-formulas.append("(a<=4)+(4<=a)")
-req_result.append(True)
+    formulas.append("(a<=4)+(4<=a)")
+    req_result.append(True)
 
-formulas.append("(a==4)*(b<=3)")
-req_result.append(True)
+    formulas.append("(a==4)*(b<=3)")
+    req_result.append(True)
 
-formulas.append("(c<1)*(1<c)")
-req_result.append(False)
+    formulas.append("(c<1)*(1<c)")
+    req_result.append(False)
 
-for i, f in enumerate(formulas):
-    print("Solving f=" + str(f))
-    results.append(smt.solve(f))
-    print("SAT("+str(i)+")= " + str(results[i][0]) + ", Assingment("+str(i)+")=" + str(results[i][1]) + "\n")
-    if results[i][0] != req_result[i]:
-        PASS = False
-        fails.add((i))
+    for i, f in enumerate(formulas):
+        print("Solving f=" + str(f))
+        results.append(smt.solve(f))
+        print("SAT("+str(i)+")= " + str(results[i][0]) + ", Assingment("+str(i)+")=" + str(results[i][1]) + "\n")
+        if results[i][0] != req_result[i]:
+            PASS = False
+            fails.add((i))
 
-if not PASS:
-    print ("FAIL")
-    print (str(fails))
-else:
-    print ("PASS")
+    if not PASS:
+        print ("FAIL")
+        print (str(fails))
+    else:
+        print ("PASS")
+
+
+if __name__ == "__main__":
+    demo()
