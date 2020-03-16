51c5c4317e273c9e6af583fc1e95b07db80a6166
shiranAziz <shiran.aziz@gmail.com>
2020-03-16 20:06:05 +0200

    ds

--- tests/test_LU_factorization.py
+++ tests/test_LU_factorization.py
@@ -7,13 +7,13 @@ def class_example_LU_treshhold_2():
                   [1,1,0],
                   [3,3,1]])
     s = LU_factorization(A)
-    eta_matrix, leaving_vars = s.run_LU_factorization()
+    p, eta_matrix, leaving_vars = s.run_LU_factorization()
 
 
     res = np.identity(eta_matrix[0].shape[0])
     for i in range(len(eta_matrix)):
         res = np.dot(res, eta_matrix[i])
-    print(A)
+    print(np.dot(p,A))
     print(res)
 
 
